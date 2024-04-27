00dd19113bd447df4f8be3641f16f151f2743653
Maksym <maksym.myronov2018@gmail.com>
2024-04-27 16:05:36 +0300

    [MP-0002] - create main cards

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchCardsFromApi } from '../../store/cardSlice';
+import { MainCards } from './components/MainCards/index';
+// Styles
+import styles from './index.module.scss';
+
+export const Main = () => {
+  const itemFromApi = useSelector((state) => state.cards.cards.cards);
+  const getFirstTenItemsFromArray = itemFromApi?.slice(0, 14);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCardsFromApi());
+  }, [dispatch]);
+
+  return (
+    <div className={styles.cards__container}>
+      {getFirstTenItemsFromArray?.map((item) => (
+        <MainCards
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          price={item.price}
+          items={item.images}
+        />
+      ))}
+    </div>
+  );
+};
